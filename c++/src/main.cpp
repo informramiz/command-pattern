1ae41ab31d72de89c4d1c267b3e217f0c0ce90f7
Ramiz Raja <informramiz@gmail.com>
2017-11-16 02:49:26 +0500

    feat: Use RemoteController to control a sample robot

--- c++/src/main.cpp
+++ c++/src/main.cpp
@@ -7,9 +7,21 @@
 //============================================================================
 
 #include <iostream>
+#include "../sdk/robot.h"
+#include "../remotecontroller/remote_controller.h"
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+  Robot robot;
+	RemoteController remote_controller(&robot);
+
+	remote_controller.MoveRobot();
+	remote_controller.TurnRobot();
+	remote_controller.BeepRobot();
+
+	//let's replay these commands
+	std::cout << endl << "Replaying commands..." << endl << endl;
+	remote_controller.Replay();
+
 	return 0;
 }
